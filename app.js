168a405087958f0183f700822836468ec8e68f01
tamer_maarouf <tamermaarouf@gmail.com>
2021-11-13 22:32:54 +0200

    Modifying CSS Quiz

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 For this quiz, use a jQuery class selector and featuredArticle variable to toggle the 'featured' class!
 
 let featuredArticle;
-
+*/
 featuredArticle = $('.article-item')						// your code starts here!
 featuredArticle.toggleClass('featured');
-*/
+
 
 /*******************************************************************************/
 
@@ -18,3 +18,18 @@ You must use jQuery's attr() method!
 var navList;
 
 navList = $('.nav-list').children().find('a').first().attr("href", "#1");               // your code goes here!
+
+/*******************************************************************************/
+
+/*									Modifying CSS Quiz 				*/
+
+/*
+For this quiz, change the font-size of all the article-items to 20px!
+
+You must use jQuery's css() method!
+*/
+
+// Start with this variable!
+var articleItems;
+
+articleItems = $('.article-item').css('font-size', '20px')// your code goes here!
