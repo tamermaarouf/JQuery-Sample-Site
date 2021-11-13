2b6426f04f4978e0a1bc8983b3ab01bd22f96510
tamer_maarouf <tamermaarouf@gmail.com>
2021-11-13 23:13:58 +0200

    Build a DOM (Family) Tree!

--- app.js
+++ app.js
@@ -67,4 +67,19 @@ articleItems = $('.article-item').children('ul').remove();// your code goes here
 
 /*---------------------------------------------------------------------------------------------------*/
 
-/*------------------------- Removing DOM Elements --------------------------------------------------*/
+/*------------------------- Build a DOM (Family) Tree! --------------------------------------------------*/
+
+/*
+For this quiz, you'll need to add to the DOM tree that already exists.
+
+'#family2' should be a sibling of and come after '#family1'. '#bruce' should be the only immediate child
+of '#family2'. '#bruce' should have two <div>s as children, '#madison' and '#hunter'.
+*/
+
+var $hunter = $(`<div id='hunter'><h3>hunter</h3></div>`);
+var $madison = $(`<div id='madison'><h3>madison</h3></div>`);
+var $bruce = $(`<div id='bruce'><h2>bruce</h2></div>`);
+$bruce.append($hunter, $madison);
+var $familyTwo = $(`<div id='family2'><h1>family2</h1></div>`)
+$familyTwo.append($bruce)
+$familyTwo.insertAfter( $('#family1') );
