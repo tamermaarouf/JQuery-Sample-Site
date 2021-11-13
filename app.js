1921c0835b4f6ea0bb79d5e885364fb51cfc14f0
tamer_maarouf <tamermaarouf@gmail.com>
2021-11-13 19:51:13 +0200

    Changing Attributes Quiz

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 /*
 For this quiz, use a jQuery class selector and featuredArticle variable to toggle the 'featured' class!
-*/
 
 let featuredArticle;
 
 featuredArticle = $('.article-item')						// your code starts here!
 featuredArticle.toggleClass('featured');
-
+*/
 
 /*******************************************************************************/
 
+/*
+For this quiz, set the href of the <a> in the first nav item to "#1".
+
+You must use jQuery's attr() method!
+*/
+
+var navList;
+
+navList = $('.nav-list').children().find('a').first().attr("href", "#1");               // your code goes here!
